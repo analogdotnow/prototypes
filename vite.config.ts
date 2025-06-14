71151c8f0f42c6fa845979eafcc49fb6ff71bbd1
Artem Popelyshev <apopelyshev@gmail.com>
2025-06-15 00:49:56 +0200

    fix: navigation issue

--- vite.config.ts
+++ vite.config.ts
@@ -26,6 +26,10 @@ const ogOptions: Options = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
   plugins: [
     react({
       babel: {
