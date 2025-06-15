6b71d8fd4816fe8e34ded81c8fd06e7b52dca836
Artem Popelyshev <apopelyshev@gmail.com>
2025-06-15 03:14:26 +0200

    feat: contribute button in example

--- src/prototypes/example/main.tsx
+++ src/prototypes/example/main.tsx
@@ -1,17 +1,29 @@
 import { TextLoop } from "@/components/ui/text-loop";
 import "./style.css";
+import { Button } from "@/components/ui/button";
 import { ShaderIcon } from "./icon";
 
 export default function App() {
   return (
-    <div className="flex flex-col flex-1 justify-center items-center w-full gap-y-40">
+    <div className="flex flex-col flex-1 justify-center items-center w-full gap-y-36">
       <ShaderIcon className="animate-rotateY-pulse" />
-      <TextLoop className="font-mono text-xl w-full text-center">
-        <span>This is a place to explore new ideas</span>
-        <span>Experiment with UI components</span>
-        <span>Collect feedback for new features</span>
-        <span>Work on crafting the best UX</span>
-      </TextLoop>
+      <div className="flex flex-col gap-y-20 items-center w-full">
+        <TextLoop className="font-mono text-xl text-center w-full">
+          <span>This is a place to explore new ideas</span>
+          <span>Experiment with UI components</span>
+          <span>Collect feedback for new features</span>
+          <span>Work on crafting the best UX</span>
+        </TextLoop>
+        <Button size="lg" variant="secondary" className="w-fit" asChild>
+          <a
+            href="https://github.com/analogdotnow/prototypes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Contribute today
+          </a>
+        </Button>
+      </div>
     </div>
   );
 }
