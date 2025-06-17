07220b641d5a1a97f1123a2cf2b0dd3b235d4ae9
Artem Popelyshev <apopelyshev@gmail.com>
2025-06-17 06:02:42 +0200

    feat: auto resizable textarea polyfill

--- src/prototypes/event-creation-card/components/form/fields/description-field.tsx
+++ src/prototypes/event-creation-card/components/form/fields/description-field.tsx
@@ -1,5 +1,6 @@
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { useAutoResizeTextarea } from "@/hooks/use-auto-resize-textarea";
 import { MessageSquare } from "lucide-react";
 import { useId } from "react";
 import { useFieldContext } from "~/event-creation-card/hooks/form-context";
@@ -7,6 +8,7 @@ import { useFieldContext } from "~/event-creation-card/hooks/form-context";
 const DescriptionField = ({ maxLength = 200 }: { maxLength?: number }) => {
   const id = useId();
   const field = useFieldContext<string>();
+  const textareaRef = useAutoResizeTextarea(120);
 
   return (
     <div className="flex gap-x-3 px-4">
@@ -15,12 +17,14 @@ const DescriptionField = ({ maxLength = 200 }: { maxLength?: number }) => {
         Event description
       </Label>
       <Textarea
+        ref={textareaRef}
         id={id}
         placeholder="Add description..."
         value={field.state.value ?? ""}
         maxLength={maxLength}
+        rows={1}
         onChange={(e) => field.handleChange(e.target.value)}
-        className="field-sizing-content max-h-29.5 min-h-0 resize-none rounded-none border-none shadow-none focus-visible:ring-0 p-0.5"
+        className="field-sizing-content scrollbar-hidden max-h-29.5 min-h-0 resize-none rounded-none border-none shadow-none focus-visible:ring-0 p-0.5"
       />
     </div>
   );
